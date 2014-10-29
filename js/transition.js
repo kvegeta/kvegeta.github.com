535db30167127af3bd2af7486f1264bb3ed66b19
Alessandro Marongiu <alessandro.marongiu.82@gmail.com>
2014-10-29 15:03:04 +0100

    first commit

--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,13 @@
-$("p").click(function() {
-	$(this).toggleClass("cc");
- });
+$(document).ready(function() {
+  $("#button a").click(function(e) {
+    $(this).parents("section").find(".pulsante").addClass("animationout").removeClass("animationin");
+    $(this).parents().siblings(".textcontent").css("visibility", "visible");
+    $('#xbutton').css("visibility", "visible");
+  });
+  
+  $("#xbutton").click(function(e) {
+    $('#xbutton').css("visibility", "hidden");
+    $("#button").parents("section").find(".pulsante").removeClass("animationout").addClass("animationin");
+    $('.textcontent').css("visibility","hidden");
+  });
+});
