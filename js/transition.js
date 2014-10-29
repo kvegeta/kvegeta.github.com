5068436ded8b23d7ae86c84eae62af0b3e67e6c4
Alessandro Marongiu <alessandro.marongiu.82@gmail.com>
2014-10-29 17:01:56 +0100

    n^n commit

--- js/transition.js
+++ js/transition.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   $("#button a").click(function(e) {
-    $(this).parents("section").find(".pulsante").addClass("animationout").removeClass("animationin");
+    $(this).parents("section").find(".menubutton").addClass("animationout").removeClass("animationin");
     $(this).parents().siblings(".textcontent").css("visibility", "visible");
     $('#xbutton').css("visibility", "visible");
   });
   
   $("#xbutton").click(function(e) {
     $('#xbutton').css("visibility", "hidden");
-    $("#button").parents("section").find(".pulsante").removeClass("animationout").addClass("animationin");
+    $("#button").parents("section").find(".menubutton").removeClass("animationout").addClass("animationin");
     $('.textcontent').css("visibility","hidden");
   });
 });
